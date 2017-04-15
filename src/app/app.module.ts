0a45d5383bd4da36ba502a8eec788b2349783c0a
mallek <travis@haleycomputersolutions.com>
2017-04-14 22:14:10 -0600

    Finished Router Guards - canActivate, canActivateChild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,19 +11,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AuthService } from "./auth.service";
+import { AuthGuard } from "./auth-guard.service";
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent }, //localhost:4200/
-  { path: 'users', component: UsersComponent }, //localhost:4200/users
-  { path: 'users/:id/:name', component: UserComponent }, //localhost:4200/users
-  {
-    //localhost:4200/servers
-    path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent}
-  ] },
-
-];
 
 @NgModule({
   declarations: [
@@ -34,15 +25,16 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+    loggedIn = false;
+    constructor() { }
+
+    isAuthenticated() {
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(function () {
+                resolve(this.loggedIn);
+            }, 800);
+        });
+        return promise;
+    }
+
+    login() {
+        this.loggedIn = true;
+    }
+
+    logOut() {
+        this.loggedIn = false;
+    }
+}
